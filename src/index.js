18c5e4b55dd577139cc904c0a9ac53fb1adff575
HaochongPan <a13126347151@gmail.com>
2022-08-22 22:56:11 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,4 +7,11 @@ import "jquery";
 import "./js/public.js";
 import "./js/nav.js";
 
-console.log("hello new project");
+// tree shaking 的触发条件:
+// 1. 通过解构方式获取方法, 然后触发tree shaking
+// 2. 调用的包必须使用按照es Module 规范 (!important)
+//    也就是通过export function a() 单独的一个function
+//    但是不能用 export default {}, 因为对象是不支持tree shaking 的
+
+import { get } from 'lodash-es'
+console.log(get({ a: 1}, 'a'))
